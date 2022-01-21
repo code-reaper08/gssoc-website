3a586e8ae0e40db7e66481327bf2c5f5fea5c7eb
MEHAK AGGARWAL <mehakagg1211@gmail.com>
2022-01-21 23:57:53 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Card from "../components/homepage/Card";
+
 export default function Home() {
   return (
     <div>
